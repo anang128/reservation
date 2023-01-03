4b654312098e73621f2c050c396f67de007b3439
anang128 <anangti2@gmail.com>
2023-01-03 13:42:52 +0700

    Code File No Fix

---
+++ src/main/java/com/example/finalproject/controller/UserController.java
@@ -0,0 +1,108 @@
+package com.example.finalproject.controller;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.finalproject.models.Bus;
+import com.example.finalproject.models.ERole;
+import com.example.finalproject.models.Role;
+import com.example.finalproject.models.Trip;
+import com.example.finalproject.models.User;
+import com.example.finalproject.payload.request.BusRequest;
+import com.example.finalproject.payload.request.SignupRequest;
+import com.example.finalproject.payload.response.MessageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.finalproject.repository.RoleRepository;
+import com.example.finalproject.repository.UserRepository;
+import com.example.finalproject.security.jwt.JwtUtils;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getFirstName(),
+							 signUpRequest.getLastName(),
+							 signUpRequest.getMobileNumber(),
+							 signUpRequest.getUsername(),
+							 signUpRequest.getEmail(),
+							 encoder.encode(signUpRequest.getPassword()));
+
+		Set<String> strRoles = signUpRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+					break;
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+		
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+	
+
+
+}
